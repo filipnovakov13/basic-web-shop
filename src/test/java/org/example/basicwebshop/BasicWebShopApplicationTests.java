d610c904a5c3eef8a1cbcdec714dcadf79c7057a
Filip Novakov <filip.novakov13@gmail.com>
2024-02-02 10:45:40 +0100

    Initial commit

---
+++ src/test/java/org/example/basicwebshop/BasicWebShopApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.basicwebshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicWebShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
